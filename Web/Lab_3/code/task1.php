eeb5d4ee2bb5cc8b9064b8457498870a07e52d77
LapKi <kirill.lapshin04@mail.ru>
2024-03-25 20:04:33 +0200

    task1. Working with regular expressions

---
+++ Web/Lab_3/code/task1.php
@@ -0,0 +1,19 @@
+<?php
+/**1. Регулярные выражения*/
+// a.
+$str = 'ahb acb aeb aeeb adcb axeb';
+$result = [];
+$regexp = '/a..b/';
+preg_match_all($regexp, $str, $result );
+echo "Найденные строки: ";
+var_dump($result);
+
+// b.
+$strB = '10 2 3';
+$regexpB = '/[0-9]+/';
+function cube($c) {
+    return $c[0] **3;
+}
+$resultB = preg_replace_callback($regexpB,"cube", $strB);
+echo $resultB;
+
