1a8b626eef9d8aead9d094c2b1d9c1a94cd1cde3
LapKi <kirill.lapshin04@mail.ru>
2024-03-26 19:20:16 +0200

    Task 2 (step 2). Working with session

--- Web/Lab_3/Task2/code/index.php
+++ Web/Lab_3/Task2/code/index.php
@@ -12,13 +12,28 @@
     STEP 1. <br>
     <div>
         <form action="step1.php" method="post">
-            <textarea aria-label="Введите текст" name="textStep1" rows="10" cols="30" ></textarea> <br>
+            <textarea name="textStep1" rows="10" cols="30" ></textarea> <br>
             <input type="submit" name="buttonStep1" value="Start"> <br>
         </form>
         <?php
         echo "Количество символов: {$_COOKIE["CountSymbols"]}<br>";
         echo "Количество слов: {$_COOKIE["CountWords"]}";
         ?>
+    </div>
+    <br>
+
+<!-- Step 2-3. Working with session -->
+    STEP 2. <br>
+    <div>
+        <form action="step2.php" method="post">
+            <label for="LastName"> Your LastName: </label>
+            <input type="text" name="LastName"> <br>
+            <label for="Name"> Your Name: </label>
+            <input type="text" name="Name"> <br>
+            <label for="Age"> Your age: </label>
+            <input type="text" name="Age"> <br>
+            <input type="submit" name="buttonStep2" value="Start"> <br>
+        </form>
     </div>
 </body>
 </html>
--- Web/Lab_3/Task2/code/step1.php
+++ Web/Lab_3/Task2/code/step1.php
@@ -3,7 +3,7 @@ function Back() {
     header("Location: /");
     exit();
 }
-if (false === isset($_POST['textStep1'])) Back();
+if (false === isset($_POST["textStep1"])) Back();
 
 setcookie("CountWords", str_word_count($_POST['textStep1']));
 setcookie("CountSymbols", strlen($_POST['textStep1']));
