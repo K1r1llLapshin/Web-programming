777969a1883ed36e4dab364aba242c9b676f3773
LapKi <kirill.lapshin04@mail.ru>
2024-05-21 21:25:57 +0200

    Implemented saving data to a database and displaying it in a table (see screenshot result_1 and result_2)

--- Web/lab_5/code/index.php
+++ Web/lab_5/code/index.php
@@ -25,7 +25,6 @@
             <label for="text" >Text</label>
             <textarea rows="5" name="text"></textarea> <br>
             <input type="submit" value="Save">
-
         </form>
     </div>
     <div id="table">
@@ -34,8 +33,29 @@
                 <th>Category</th>
                 <th>Title</th>
                 <th>Description</th>
+                <th>Email</th>
             </thead>
             <tbody>
+            <?php
+                /* Подключение к серверу*/
+                $mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+                if($result = $mysqli->query('SELECT * FROM ad')){
+                    while ($row = $result->fetch_assoc()){
+                        $category = $row['category'];
+                        $title = $row['title'];
+                        $email = $row['email'];
+                        $text = $row['description'];
+                        echo "<tr>";
+                        echo "<td>$category</td>";
+                        echo "<td>$title</td>";
+                        echo "<td>$text</td>";
+                        echo "<td>$email</td>";
+                        echo "</tr>";
+                    }
+                }
+
+            ?>
             </tbody>
         </table>
     </div>
--- Web/lab_5/code/save.php
+++ Web/lab_5/code/save.php
@@ -6,10 +6,23 @@ function Back() {
 
 if (false === isset($_POST["email"], $_POST["categories"], $_POST["title"], $_POST["text"])) Back();
 
-$caregory = $_POST["categories"];
 $title = $_POST["title"];
 $text = $_POST["text"];
 $email = $_POST["email"];
+$category = $_POST["categories"];
+
+/* Подключение к серверу*/
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_error()){
+    printf("Подключение к серверу MySQL невозможно, Код ошибки: %s\n", mysqli_connect_error());
+    Back();
+}
+
+$mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$text', '$category')");
+$mysqli->close();
+
+
 
 Back();
 
